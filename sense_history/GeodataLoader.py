0d98f2a2ba98d42ae1baeab490e879ea47f8a520
tilana <natalie_widmann@posteo.net>
2019-10-10 21:22:00 +0100

    load geodata based on modules / functionality to load and process json data

--- sense_history/GeodataLoader.py
+++ sense_history/GeodataLoader.py
@@ -1,13 +1,55 @@
 from shapely.geometry import shape
+from functools import reduce
+import operator
 import json
+import yaml
 
 
 class GeodataLoader(object):
 
-    def __init__(self):
-        pass
+    MODULES_CONFIG = './module/config.yml'
+    MODULES = ['BERLIN_WALL', 'ZURICH_TREES']
 
-    def load_polygon(self, path):
-        with open(path) as f:
+    def __init__(self, MODULE_NAME):
+        assert MODULE_NAME in self.MODULES
+        with open(self.MODULES_CONFIG) as f:
+            self.cfg = yaml.load(f, Loader=yaml.FullLoader)[MODULE_NAME]
+        self.load_module(MODULE_NAME)
+        
+
+    def load_module(self, MODULE):
+        if MODULE == 'BERLIN_WALL':
+            self.data = self.load_polygon()
+        if MODULE == 'ZURICH_TREES':
+            self.data = self.load_json()
+
+    def load_json(self):
+        data = self.load_json(self.cfg['path'])[self.cfg['key']]
+        data = data[:100]
+        features = [self.extract_features(record) for record in data]
+
+    def load_polygon(self):
+        with open(self.cfg['path']) as f:
             data = json.load(f)
         return shape(data['features'][0]['geometry'])
+
+    def load_json(self, path):
+        with open(path) as f:
+            data = json.load(f)
+        return data
+
+    def extract_features(self, record):
+        latitude = self.get_from_dictionary(record, self.cfg['latitude'])
+        longitude = self.get_from_dictionary(record, self.cfg['longitude'])
+        features = {} 
+        for feature in self.cfg['features']:
+            properties = self.get_from_dictionary(record, feature.values()[0])
+            features.update({feature.keys()[0]: properties})
+
+    def get_from_dictionary(self, data, keys):
+        return reduce(operator.getitem, keys, data)
+
+
+
+
+
