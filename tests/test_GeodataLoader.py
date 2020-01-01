ecad0a4b7c701126e77910415e57759a9db7ba5c
tilana <natalie_widmann@posteo.net>
2020-01-01 16:27:48 +0000

    tests for GeodataLoader / BorderCrossing

--- tests/test_GeodataLoader.py
+++ tests/test_GeodataLoader.py
@@ -1,28 +1,24 @@
 import unittest
 from sense_history.GeodataLoader import GeodataLoader 
-from shapely.geometry import Point
+
 
 class TestGeodataLoader(unittest.TestCase):
 
     def setUp(self):
-        path = './module/berlin-wall/polygon.geojson'
-        self.berlin_wall = GeodataLoader().load_polygon(path)
+        self.loader = GeodataLoader('TEST')
 
-    def test_contains(self):
-        fernsehturm = Point(13.409779, 52.520645)
-        self.assertFalse(self.berlin_wall.contains(fernsehturm))
-        stuttgart = Point(9.176514, 48.773079)
-        self.assertFalse(self.berlin_wall.contains(stuttgart))
-        siegessaeule = Point(13.350119, 52.514543)
-        self.assertTrue(self.berlin_wall.contains(siegessaeule))
-        westend = Point(13.259284, 52.508645)
-        self.assertTrue(self.berlin_wall.contains(westend))
-        checkpoint_charlie_west = Point(13.390391, 52.507443)
-        checkpoint_charlie_east = Point(13.390391, 52.508043)
-        self.assertTrue(self.berlin_wall.contains(checkpoint_charlie_west))
-        self.assertFalse(self.berlin_wall.contains(checkpoint_charlie_east))
+    def test_getFromDictionary(self):
+        data = {'geometry': {'type': 'Point', 'coordinates': [42.00, 12.00]},
+                'properties': {'name': 'POI', 'year': '1900'}}
 
-        #self.assertTrue(self.berlin_wall.almost_equals(checkpoint_charlie_east, 1))
-        #self.assertTrue(self.berlin_wall.almost_equals(checkpoint_charlie_west))
+        LNG_keys = ['geometry', 'coordinates', '0']
+        name_keys = ['properties', 'name']
+        self.assertEqual(self.loader.get_from_dictionary(data, name_keys), 'POI')
+        self.assertEqual(self.loader.get_from_dictionary(data, ['geometry']), {'type': 'Point', 'coordinates': [42.00, 12.00]})
+        self.assertEqual(self.loader.get_from_dictionary(data, ['geometry', 'coordinates']), [42.00, 12.00])
+        self.assertEqual(self.loader.get_from_dictionary(data, LNG_keys), 42.00)
 
+        
 
+if __name__ == '__main__':
+    unittest.main()
