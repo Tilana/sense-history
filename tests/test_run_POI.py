bd9518b7d7e9193009a83c9c06f717ae17d1cadb
Natalie <natalie@huridocs.org>
2020-05-24 09:51:21 +0200

    add test POI module

--- tests/test_run_POI.py
+++ tests/test_run_POI.py
@@ -1,37 +1,30 @@
 import unittest
-from sense_history.GeodataLoader import GeodataLoader 
+from sense_history.GeodataLoader import GeodataLoader
 from shapely.geometry import Point
 
 class TestRunConfig(unittest.TestCase):
 
     def setUp(self):
-        MODULE = 'BERLIN_HISTORIC'
+        MODULE = 'TEST'
         self.POIs = GeodataLoader(MODULE).POIs
 
+    # TODO:
+    # Requirements:
+    # (1) Vibrate if close to POI
+    # (2) Stop vibrating if constantly within radius of POI
+    # (3) If out of radius of POI and in again vibrate
+
+    def test_module(self):
+        self.assertEqual(len(self.POIs), 5)
+        names = [poi.name for poi in self.POIs]
+        self.assertIn('Zuse Z3', names)
+
     def test(self):
-        luftbrueckendenkmal = Point(13.3874049, 52.474135)
-        
-        distances = [poi.distance(luftbrueckendenkmal) for poi in self.POIs]
-        print(distances[:10])
-        distances.sort()
-        print(distances[:10])
+        zuse_location = [poi.location for poi in self.POIs if poi.name == 'Zuse Z3'][0]
+        distances = [poi.distance(zuse_location) for poi in self.POIs]
+        self.assertIn(0, distances)
+        for dist in distances:
+            if dist != 0:
+                self.assertGreaterEqual(dist, 3000)
         close_POIs = [distance for distance in distances if distance <= 5]
-
-
-        #self.assertEqual(len(close_POIs), 1)
-        #fernsehturm = Point(13.409779, 52.520645)
-        #self.assertFalse(self.berlin_wall.contains(fernsehturm))
-        #stuttgart = Point(9.176514, 48.773079)
-        #self.assertFalse(self.berlin_wall.contains(stuttgart))
-        #siegessaeule = Point(13.350119, 52.514543)
-        #self.assertTrue(self.berlin_wall.contains(siegessaeule))
-        #westend = Point(13.259284, 52.508645)
-        #self.assertTrue(self.berlin_wall.contains(westend))
-        #checkpoint_charlie_west = Point(13.390391, 52.507443)
-        #checkpoint_charlie_east = Point(13.390391, 52.508043)
-        #self.assertTrue(self.berlin_wall.contains(checkpoint_charlie_west))
-        #self.assertFalse(self.berlin_wall.contains(checkpoint_charlie_east))
-
-        #self.assertTrue(self.berlin_wall.almost_equals(checkpoint_charlie_east, 1))
-        #self.assertTrue(self.berlin_wall.almost_equals(checkpoint_charlie_west))
-
+        self.assertEqual(len(close_POIs), 1)
